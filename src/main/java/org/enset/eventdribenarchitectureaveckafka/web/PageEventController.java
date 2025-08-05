76a011af3282a8c1c55e41e201ae677d063e0963
Mourad Ouammou <mouradouammou8@gmail.com>
2025-08-05 19:10:57 +0100

    PageEvent Added

---
+++ src/main/java/org/enset/eventdribenarchitectureaveckafka/web/PageEventController.java
@@ -0,0 +1,20 @@
+package org.enset.eventdribenarchitectureaveckafka.web;
+
+import org.enset.eventdribenarchitectureaveckafka.entities.PageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageEventController {
+    @Autowired
+    private StreamBridge streamBridge;
+    @GetMapping("/publish/{topic}/{name}")
+    public PageEvent publish(@PathVariable String topic, @PathVariable String name) {
+        PageEvent pageEvent = new PageEvent(name, "user1", new java.util.Date(), 1000);
+        streamBridge.send("topic",pageEvent);
+        return  PageEvent;
+    }
+}
