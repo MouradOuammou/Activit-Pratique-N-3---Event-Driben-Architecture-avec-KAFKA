d4d23c7931c10942423e5ef085ef8016d6693536
Mourad Ouammou <mouradouammou8@gmail.com>
2025-08-05 19:01:48 +0100

    fixer dep

--- src/main/java/org/enset/eventdribenarchitectureaveckafka/EventDribenArchitectureavecKafkaApplication.java
+++ src/main/java/org/enset/eventdribenarchitectureaveckafka/EventDribenArchitectureavecKafkaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventDribenArchitectureavecKafkaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventDribenArchitectureavecKafkaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventDribenArchitectureavecKafkaApplication.class, args);
+    }
 
 }
--- src/test/java/org/enset/eventdribenarchitectureaveckafka/EventDribenArchitectureavecKafkaApplicationTests.java
+++ src/test/java/org/enset/eventdribenarchitectureaveckafka/EventDribenArchitectureavecKafkaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EventDribenArchitectureavecKafkaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
